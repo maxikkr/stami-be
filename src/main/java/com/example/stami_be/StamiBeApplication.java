b432cf106f79c7c4f04dd03bfcb64ee431c2d345
maxikkr <104993102+maxikkr@users.noreply.github.com>
2025-04-26 00:08:01 +0200

    initial commit

---
+++ src/main/java/com/example/stami_be/StamiBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.stami_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamiBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StamiBeApplication.class, args);
+	}
+
+}
